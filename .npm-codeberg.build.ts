879faaa64b446fbba9e9efd605edfe165c2f4bbd
hugoalh <hugoalh@gmail.com>
2026-07-04 17:10:32 +0800

    Update resources

---
+++ .npm-codeberg.build.ts
@@ -0,0 +1,42 @@
+import { invokeDenoNodeJSTransformer } from "DNT";
+import { parse as parseJSONC } from "STD_JSONC";
+const jsrManifest = parseJSONC(await Deno.readTextFile(new URL(import.meta.resolve("./jsr.jsonc"))));
+await invokeDenoNodeJSTransformer({
+	copyEntries: [
+		"LICENSE.md",
+		"README.md"
+	],
+	//@ts-ignore Lazy type.
+	entrypointsScript: jsrManifest.exports,
+	generateDeclarationMap: true,
+	mappings: {
+	},
+	metadata: {
+		//@ts-ignore Lazy type.
+		name: jsrManifest.name,
+		//@ts-ignore Lazy type.
+		version: jsrManifest.version,
+		description: "A module for URL regular expression.",
+		keywords: [
+			"regexp",
+			"regular-expression",
+			"url"
+		],
+		homepage: "https://codeberg.org/hugoalh/url-regexp-es#readme",
+		bugs: {
+			url: "https://codeberg.org/hugoalh/url-regexp-es/issues"
+		},
+		license: "MIT",
+		author: "hugoalh",
+		repository: {
+			type: "git",
+			url: "git+https://codeberg.org/hugoalh/url-regexp-es.git"
+		},
+		private: false,
+		publishConfig: {
+			access: "public"
+		}
+	},
+	outputDirectory: "dist/npm-codeberg",
+	outputDirectoryPreEmpty: true
+});
